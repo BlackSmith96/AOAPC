9982a5380690e130e8b58fd45bda27b85096e952
blacksmith96 <blacksmith96@yahoo.com>
2018-07-22 21:23:11 -0400

    Initial commit

---
+++ MINE/Uva725/cmake-build-debug/CMakeFiles/Uva725.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uva725.dir/main.cpp.o"
+  "Uva725.pdb"
+  "Uva725"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Uva725.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
